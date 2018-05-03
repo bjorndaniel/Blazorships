a18b249c8492e3e3ebe1e65d3c7938822a0ea7f2
bjorndaniel <bjorndaniel@gmail.com>
2018-05-03 21:51:10 +0200

    Added Blazor client and server Added SignalR

---
+++ Blazorships/Blazorships.Client/JsInterop.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace Blazorships.Client
+{
+    public class JsInterop
+    {
+        public static string InitChat()
+        {
+            return RegisteredFunction.Invoke<string>(
+                "Blazorships.Client.JsInterop.InitChat");
+
+        }
+    }
+}
---
+++ Blazorships/Blazorships.Client/wwwroot/js/chat.js
@@ -0,0 +1,22 @@
+﻿Blazor.registerFunction('Blazorships.Client.JsInterop.InitChat', function () {
+
+    const connection = new signalR.HubConnection(
+        "/chathub", { logger: signalR.LogLevel.Information });
+
+    connection.on("ReceiveMessage", (user, message) => {
+        const encodedMsg = user + " says " + message;
+        const li = document.createElement("li");
+        li.textContent = encodedMsg;
+        document.getElementById("messagesList").appendChild(li);
+    });
+
+    document.getElementById("sendButton").addEventListener("click", event => {
+        const user = document.getElementById("userInput").value;
+        const message = document.getElementById("messageInput").value;
+        connection.invoke("SendMessage", user, message).catch(err => console.error);
+        event.preventDefault();
+    });
+
+    connection.start().catch(err => console.error);
+
+});
